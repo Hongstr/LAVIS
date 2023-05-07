4374e7a86157bd9d23d281a5b3e2ecd12dac48e8
coro <honghongon@gmail.com>
2023-05-07 04:39:57 -0700

    train v1.4

--- run_scripts/blip2/train/train_caption_ui.sh
+++ run_scripts/blip2/train/train_caption_ui.sh
@@ -1,1 +1,3 @@
-python -m torch.distributed.run --nproc_per_node=1 train.py --cfg-path lavis/projects/blip2/train/ui/caption_ui_ft.yaml
+# python -m torch.distributed.run --nproc_per_node=1 train.py --cfg-path lavis/projects/blip2/train/ui/caption_ui_ft.yaml
+python -m torch.distributed.run --nproc_per_node=1 train.py --cfg-path lavis/projects/blip2/train/ui/train_v1.3.yaml
+python -m torch.distributed.run --nproc_per_node=1 train.py --cfg-path lavis/projects/blip2/train/ui/train_v1.4.yaml
